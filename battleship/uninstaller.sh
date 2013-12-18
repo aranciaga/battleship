5db58f0e0ea28e21a758dabedf0dcf3c0410af2c
wlan0 <martin_aranciaga@hotmail.com>
2013-12-17 23:20:20 -0300

    New version

--- battleship/uninstaller.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-echo "Uninstalling battleship..."
-sudo make uninstall
-sudo rm /usr/share/applications/battleship.desktop
-sudo rm -r /usr/share/battleship
-sudo xdg-desktop-menu forceupdate 
-echo "done."
